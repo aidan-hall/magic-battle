1d7bc8a6fe50e3e16d380a4402cf6f8e320f1a1f
Aidan Hall <aidan.hall202@gmail.com>
2023-01-30 10:43:32 +0000

    [main.cpp] Small changes; fixed sprite offset

--- source/main.cpp
+++ source/main.cpp
@@ -1,10 +1,5 @@
-#include "nds/arm9/math.h"
 #include "nds/arm9/sprite.h"
-#include "nds/arm9/trig_lut.h"
 #include "nds/arm9/video.h"
-#include "nds/dma.h"
-#include "nds/input.h"
-#include "nds/touch.h"
 #include "ndspp.hpp"
 #include "tecs-system.hpp"
 #include "tecs.hpp"
@@ -12,6 +7,7 @@
 #include <algorithm>
 #include <alien1.h>
 #include <array>
+#include <cassert>
 #include <cstdint>
 #include <gl2d.h>
 #include <nds.h>
@@ -20,8 +16,8 @@
 #include <stdio.h>
 #include <unordered_set>
 
-unusual::id_manager<int, 128> sprite_id_manager;
-unusual::id_manager<int, 32> affine_index_manager;
+unusual::id_manager<int, SPRITE_COUNT> sprite_id_manager;
+unusual::id_manager<int, MATRIX_COUNT> affine_index_manager;
 
 using namespace nds;
 
@@ -43,19 +39,68 @@ struct Velocity {
 struct SpriteInfo {
   int id;
   int affine;
-  int palette_index;
+  // Width divided by 2
+  fix width2;
+  // Height divided by 2
+  fix height2;
 };
 
 struct Alien {};
+
+constexpr SpriteSize sprite_size(int width, int height) {
+  switch (width) {
+  case 8:
+    switch (height) {
+    case 8:
+      return SpriteSize_8x8;
+    case 16:
+      return SpriteSize_8x16;
+    case 32:
+      return SpriteSize_8x32;
+    }
+  case 16:
+    switch (height) {
+    case 8:
+      return SpriteSize_16x8;
+    case 16:
+      return SpriteSize_16x16;
+    case 32:
+      return SpriteSize_16x32;
+    }
+  case 32:
+    switch (height) {
+    case 8:
+      return SpriteSize_32x8;
+    case 16:
+      return SpriteSize_32x16;
+    case 32:
+      return SpriteSize_32x32;
+    case 64:
+      return SpriteSize_32x64;
+    }
+  case 64:
+    switch (height) {
+    case 32:
+      return SpriteSize_64x32;
+    case 64:
+      return SpriteSize_64x64;
+    }
+  default:
+    // Invalid width-height combination.
+    assert(false);
+  }
+}
 
 using namespace Tecs;
 
 void make_sprite(Coordinator &ecs, Entity entity, void *graphics_offset,
                  SpriteSize size, SpriteColorFormat color_format,
-                 int palette_index) {
+                 int palette_index, int width, int height) {
   ecs.addComponent<SpriteInfo>(entity);
   const SpriteInfo &sprite_info = ecs.getComponent<SpriteInfo>(entity) =
-      SpriteInfo{sprite_id_manager.allocate(), affine_index_manager.allocate()};
+      SpriteInfo{sprite_id_manager.allocate(), affine_index_manager.allocate(),
+                 // Width and height are doubled to allow room for rotation
+                 fix::from_int(width), fix::from_int(height)};
   // oamSetGfx(&oamMain, sprite_id.value, size, color_format, graphics_offset);
   oamSet(&oamMain, sprite_info.id, 5, 5, 0, palette_index, size, color_format,
          graphics_offset, sprite_info.affine, true, false, false, false, false);
@@ -66,8 +111,8 @@ void draw_sprites(Coordinator &ecs,
   for (const Entity entity : entities) {
     const auto id = ecs.getComponent<SpriteInfo>(entity);
     const auto transform = ecs.getComponent<Transform>(entity);
-    oamSetXY(&oamMain, id.id, static_cast<int32_t>(transform.pos.x),
-             static_cast<int32_t>(transform.pos.y));
+    oamSetXY(&oamMain, id.id, static_cast<int32_t>(transform.pos.x - id.width2),
+             static_cast<int32_t>(transform.pos.y - id.height2));
     oamRotateScale(&oamMain, id.affine, transform.rotation, 1 << 8, 1 << 8);
   }
 }
@@ -156,9 +201,11 @@ int main(void) {
   oamInit(&oamMain, SpriteMapping_1D_32, true);
 
   u16 *alien_gfx;
-  alien_gfx =
-      oamAllocateGfx(&oamMain, SpriteSize_32x32, SpriteColorFormat_256Color);
-  dmaCopy(alien1Tiles, alien_gfx, 32 * 32);
+  constexpr int alien_width = 32;
+  constexpr int alien_height = 32;
+  constexpr SpriteSize alien_size = sprite_size(alien_width, alien_height);
+  alien_gfx = oamAllocateGfx(&oamMain, alien_size, SpriteColorFormat_256Color);
+  dmaCopy(alien1Tiles, alien_gfx, SPRITE_SIZE_PIXELS(alien_size));
 
   // Load palettes
   vramSetBankF(VRAM_F_LCD);
@@ -171,7 +218,7 @@ int main(void) {
   const u8 *alien1TilesU8 = reinterpret_cast<const u8 *>(alien1Tiles);
   const u8 *wiggly = alien1TilesU8 + 32 * 32;
 
-  std::array<Tecs::Entity, 5> aliens;
+  std::array<Tecs::Entity, 10> aliens;
   std::for_each(aliens.begin(), aliens.end(),
                 [&ecs](auto &e) { e = ecs.newEntity(); });
   {
@@ -179,8 +226,8 @@ int main(void) {
     int y = 10;
     int a = degreesToAngle(0);
     for (const auto alien : aliens) {
-      make_sprite(ecs, alien, alien_gfx, SpriteSize_32x32,
-                  SpriteColorFormat_256Color, 0);
+      make_sprite(ecs, alien, alien_gfx, alien_size, SpriteColorFormat_256Color,
+                  0, alien_width, alien_height);
       ecs.addComponent<Alien>(alien);
       ecs.addComponent<Transform>(alien);
       ecs.getComponent<Transform>(alien) =
@@ -199,14 +246,17 @@ int main(void) {
   ecs.addComponents(player, Transform{Vec3{fix::from_int(SCREEN_WIDTH / 2),
                                            fix::from_int(SCREEN_HEIGHT / 2)},
                                       0});
+  constexpr int player_width = 32;
+  constexpr int player_height = 32;
+  constexpr SpriteSize player_size = sprite_size(player_width, player_height);
   u16 *player_gfx =
-      oamAllocateGfx(&oamMain, SpriteSize_32x32, SpriteColorFormat_256Color);
-  dmaCopy(playerTiles, player_gfx, 32 * 32);
+      oamAllocateGfx(&oamMain, player_size, SpriteColorFormat_256Color);
+  dmaCopy(playerTiles, player_gfx, SPRITE_SIZE_PIXELS(player_size));
 
   // dmaCopy(playerPal, &VRAM_F_EXT_SPR_PALETTE[0][0], playerPalLen);
 
-  make_sprite(ecs, player, player_gfx, SpriteSize_32x32,
-              SpriteColorFormat_256Color, 1);
+  make_sprite(ecs, player, player_gfx, player_size, SpriteColorFormat_256Color,
+              1, player_width, player_height);
 
   // auto &position = ecs.getComponent<Position>(aliens[1]);
   // position.x = nds::fix(40);
@@ -231,10 +281,15 @@ int main(void) {
     if (held & KEY_START)
       break;
     runSystems(ecs, physics_system_interest);
+
+    printf("\x1b[10;0HTouch x = %04i, %04i\n", touch_position.rawx,
+           touch_position.px);
+    printf("Touch y = %04i, %04i\n", touch_position.rawy, touch_position.py);
+
     if (held & KEY_TOUCH) {
       Vec3 &position = ecs.getComponent<Transform>(player).pos;
-      position.x = fix::from_int(touch_position.px - 32);
-      position.y = fix::from_int(touch_position.py - 32);
+      position.x = fix::from_int(touch_position.px);
+      position.y = fix::from_int(touch_position.py);
     }
   }
 }
