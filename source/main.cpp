3c5533740c0316dc089bfb49acf398293228cbba
Aidan Hall <aidan.hall202@gmail.com>
2023-02-05 18:19:21 +0000

    [main.cpp] Randomly spawning zombies

--- source/main.cpp
+++ source/main.cpp
@@ -17,6 +17,7 @@
 #include <array>
 #include <cassert>
 #include <cstdint>
+#include <cstdlib>
 #include <gl2d.h>
 #include <nds.h>
 #include <nds/arm9/console.h>
@@ -50,6 +51,11 @@ constexpr nds::fix MAGIC_BUILD_RATE = nds::fix::from_float(0.3f);
 constexpr nds::fix FIREBALL_MAGIC = nds::fix::from_float(25.0f);
 constexpr nds::fix TELEPORT_MAGIC = nds::fix::from_float(50.0f);
 nds::fix magic_meter = MAX_MAGIC;
+
+constexpr nds::fix FIX_SCREEN_WIDTH = nds::fix::from_int(SCREEN_WIDTH);
+constexpr nds::fix FIX_SCREEN_HEIGHT = nds::fix::from_int(SCREEN_HEIGHT);
+
+constexpr nds::fix ZOMBIE_SPEED = nds::fix::from_float(0.25f);
 
 using namespace nds;
 
@@ -176,10 +182,9 @@ int main(void) {
     int x = SCREEN_WIDTH - 5;
     int y = 10;
 
-    for (int i = 0; i < 20; ++i) {
+    for (int i = 0; i < 3; ++i) {
       make_zombie(ecs, Vec3{nds::fix::from_int(x), nds::fix::from_int(y), {}},
-                  player, nds::fix::from_float(0.25f), sprite_id_manager,
-                  zombie_sprite);
+                  player, ZOMBIE_SPEED, sprite_id_manager, zombie_sprite);
       x -= 20;
       y += 30;
     }
@@ -242,6 +247,35 @@ int main(void) {
            static_cast<float>(magic_meter), static_cast<float>(FIREBALL_MAGIC),
            static_cast<float>(TELEPORT_MAGIC),
            spell_strings.at(selected_spell));
+
+    // Randomly spawn a zombie
+    if (rand() < 0.015f * RAND_MAX) {
+      constexpr nds::fix OFFSCREEN_MARGIN = nds::fix::from_float(5.0f);
+      Vec3 zombie_position = {};
+      switch (rand() % 4) {
+      case 0:
+        // on the left
+        zombie_position.x = nds::fix{0} - OFFSCREEN_MARGIN;
+        zombie_position.y = nds::fix::from_int(rand() % SCREEN_HEIGHT);
+        break;
+      case 1:
+        // on the right
+        zombie_position.x = FIX_SCREEN_WIDTH + OFFSCREEN_MARGIN;
+        zombie_position.y = nds::fix::from_int(rand() % SCREEN_HEIGHT);
+        break;
+      case 2:
+        // on the top
+        zombie_position.x = nds::fix::from_int(rand() % SCREEN_WIDTH);
+        zombie_position.y = nds::fix{0} - OFFSCREEN_MARGIN;
+        break;
+      case 3:
+        // on the bottom
+        zombie_position.x = nds::fix::from_int(rand() % SCREEN_WIDTH);
+        zombie_position.y = FIX_SCREEN_HEIGHT + OFFSCREEN_MARGIN;
+        break;
+      }
+      make_zombie(ecs, zombie_position, player, ZOMBIE_SPEED, sprite_id_manager, zombie_sprite);
+    }
 
     runSystems(ecs, admin_system_interest);
     ecs.destroyQueued();
